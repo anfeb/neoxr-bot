1ff7f9587fb18c38b919922f6fecdf55fa41970a
anfeb <88554978+anfeb@users.noreply.github.com>
2022-02-03 00:07:13 +0700

    Update neoxrApi.js

--- system/neoxrApi.js
+++ system/neoxrApi.js
@@ -7,37 +7,37 @@ class NeoxrApi {
    }
 
    async ig(url) {
-      let json = await Func.fetchJson(this.baseUrl + '/ig?url=' + url + '&apikey=' + this.apiKey)
+      let json = await Func.fetchJson(this.baseUrl + '/ig?url=' + url + '&IEAZN1H1wN=' + this.apiKey)
       return json
    }
 
    async igs(url) {
-      let json = await Func.fetchJson(this.baseUrl + '/igs2?q=' + Func.igFixed(url) + '&apikey=' + this.apiKey)
+      let json = await Func.fetchJson(this.baseUrl + '/igs2?q=' + Func.igFixed(url) + '&IEAZN1H1wN=' + this.apiKey)
       return json
    }
 
    async pin(url) {
-      let json = await Func.fetchJson(this.baseUrl + '/pin?url=' + url + '&apikey=' + this.apiKey)
+      let json = await Func.fetchJson(this.baseUrl + '/pin?url=' + url + '&IEAZN1H1wN=' + this.apiKey)
       return json
    }
 
    async mediafire(url) {
-      let json = await Func.fetchJson(this.baseUrl + '/mediafire?url=' + url + '&apikey=' + this.apiKey)
+      let json = await Func.fetchJson(this.baseUrl + '/mediafire?url=' + url + '&IEAZN1H1wN=' + this.apiKey)
       return json
    }
 
    async tiktok(url) {
-      let json = await Func.fetchJson(this.baseUrl + '/tiktok?url=' + url + '&apikey=' + this.apiKey)
+      let json = await Func.fetchJson(this.baseUrl + '/tiktok?url=' + url + '&IEAZN1H1wN=' + this.apiKey)
       return json
    }
 
    async twitter(url) {
-      let json = await Func.fetchJson(this.baseUrl + '/twitter?url=' + url + '&apikey=' + this.apiKey)
+      let json = await Func.fetchJson(this.baseUrl + '/twitter?url=' + url + '&IEAZN1H1wN=' + this.apiKey)
       return json
    }
 
    async soundcloud(url) {
-      let json = await Func.fetchJson(this.baseUrl + '/soundcloud?url=' + url + '&apikey=' + this.apiKey)
+      let json = await Func.fetchJson(this.baseUrl + '/soundcloud?url=' + url + '&IEAZN1H1wN=' + this.apiKey)
       return json
    }
 }
